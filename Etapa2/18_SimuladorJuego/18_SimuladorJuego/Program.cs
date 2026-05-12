53811ccb0f98ba059f9543d46740db5c7498947a
Matias Mamani <matyescolar391@gmail.com>
2026-05-12 11:31:12 -0300

    subo progreso tp 18 etapa2

--- Etapa2/18_SimuladorJuego/18_SimuladorJuego/Program.cs
+++ Etapa2/18_SimuladorJuego/18_SimuladorJuego/Program.cs
@@ -22,9 +22,13 @@ namespace _18_SimuladorJuego
             int material = 0;
             int busquedaComida;
             int exploracion;
-            while(opcion != 8)
+            bool siguienteDia;
+            bool derrota = false;
+            bool victoria = false;
+            while(opcion != 8 && derrota == false && victoria == false)
             {
                 opcion = 0;
+                siguienteDia = true;
                 Console.WriteLine("------------------------");
                 Console.WriteLine("SUPERVIVENCIA EN LA ISLA");
                 Console.WriteLine("------------------------");
@@ -43,6 +47,17 @@ namespace _18_SimuladorJuego
                 Console.WriteLine("6.Comer comida cocida");
                 Console.WriteLine("7.Descansar");
                 Console.WriteLine("8.Salir");
+                if (vida == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("PERDISTE");
+                    derrota = true;
+                }
+                if (dias >= 10 && vida == 10 && hambre == 10)
+                {
+                    Console.WriteLine("¡Has logrado sobrevvir 10 días!");
+                    victoria = true;
+                }
                 opcion = int.Parse(Console.ReadLine());
                 
                 switch (opcion)
@@ -62,7 +77,10 @@ namespace _18_SimuladorJuego
                             Console.WriteLine("No pudiste encontrar nada");
                             Console.WriteLine("Ir a buscar comida te consume energía, pierdes 1 punto vida");
                         }
-                        vida--;
+                        if (vida > 0)
+                        {
+                            vida--;
+                        }
                         break;
                     case 2:
                         exploracion = rand.Next(1, 10);
@@ -82,6 +100,7 @@ namespace _18_SimuladorJuego
                         {
                             Console.WriteLine("Sufriste un accidente durante la exploración");
                             Console.WriteLine("Pierdes 2 puntos de vida");
+                            vida = vida - 2;
                         }
                         break;
                     case 3:
@@ -93,27 +112,122 @@ namespace _18_SimuladorJuego
                         else if (refugio == true)
                         {
                             Console.WriteLine("No es necesario construir un refugio");
+                            siguienteDia = false;
                         }
                         else
                         {
                             Console.WriteLine("No tienes suficiente material para construir un refugio");
+                            siguienteDia = false;
                         }
                            
                         break;
+                    case 4:
+                        if (material >= 4)
+                        {
+                            fogata = true;
+                            Console.WriteLine("Lograste encender una fogata");
+                            Console.WriteLine("Encender la fogata te costó 4 materiales");
+                            material = material - 4;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No tienes suficiente material para encender una fogata");
+                            siguienteDia = false;
+                        }
+                        break;
+                    case 5:
+                        if (fogata == true && comidaCruda >= 1)
+                        {
+                            Console.WriteLine("Cocinas la comida cruda");
+                            Console.WriteLine("Se te resta 1 de comida cruda y obtienes 1 de comida cocida");
+                            comidaCruda--;
+                            comidaCocida++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Necesitas una fogata encendida y al menos 1 pedazo de comida cruda para poder cocinar");
+                            siguienteDia = false;
+                        }
+                        break;
+                    case 6:
+                        siguienteDia = false;
+                        if (comidaCocida >= 1)
+                        {
+                            Console.WriteLine("Comes comida cocida");
+                            Console.WriteLine("Se te resta 1 de comida cocida");
+                            Console.WriteLine("Recuperas 4 puntos de hambre");
+                            comidaCocida--;
+                            if (hambre <= 5)
+                            {
+                                hambre = hambre + 4;
+                            }
+                            else if (hambre >= 6 && hambre < 10)
+                            {
+                                while (hambre < 10)
+                                {
+                                    hambre++;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No tienes comida cocida para comer");
+                        }
+                        break;
+                    case 7:
+                        if (refugio == true)
+                        {
+                            Console.WriteLine("Descansas en el refugio");
+                            Console.WriteLine("Recuperas 3 puntos de vida");
+                            if (vida <= 6)
+                            {
+                                vida = vida + 3;
+                            }
+                            else if (vida >= 7 && vida < 10)
+                            {
+                                while (vida < 10)
+                                {
+                                    vida++;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Descansas");
+                            Console.WriteLine("Recuperas 1 punto de vida");
+                            if (vida < 10)
+                            {
+                                vida++;
+                            }
+                        }
+                        break;
+                    case 8:
+                        Console.WriteLine("Cerrando juego...");
+                        break;
+                    default:
+                        Console.WriteLine("Elija una opción válida");
+                        siguienteDia = false;
+                        break;
                 }
-                dias++;
-                if (hambre == 0)
+                if (siguienteDia == true)
                 {
-                    vida--;
+                    dias++;
+                    if (hambre == 0)
+                    {
+                        if (vida > 0)
+                        {
+                            vida--;
+                        }
+                    }
+                    else if (hambre > 0)
+                    {
+                        hambre = hambre - 2;
+                    }
                 }
-                else if (hambre > 0)
-                {
-                    hambre = hambre - 2;
-                }
+                
                 Console.ReadKey();
                 Console.Clear();
             }
- 
             //Console.WriteLine(num);
             
 
