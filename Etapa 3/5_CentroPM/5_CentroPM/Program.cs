117a9f2a6ef6d72bad9eef87f17840d3ac2046f9
Matias Mamani <matyescolar391@gmail.com>
2026-06-23 11:28:08 -0300

    Subo progreso tp 5 etapa 3

--- Etapa 3/5_CentroPM/5_CentroPM/Program.cs
+++ Etapa 3/5_CentroPM/5_CentroPM/Program.cs
@@ -12,23 +12,59 @@ namespace _5_CentroPM
         {
             int opcion = 0;
             int[] vidasPokemones = new int[6];
+            int enLista = 0;
+            int siguiente = 0;
             while (opcion < 13)
             {
+                opcion = 0;
                 Console.WriteLine("------------------------------------");
                 Console.WriteLine("C E N T R O  P O K É - R E M E D I O");
                 Console.WriteLine("------------------------------------");
-
+                Console.WriteLine("Elija una opcióbn: ");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("1- Registrar un nuevo pokemón");
+                Console.WriteLine("2- Mostrar la vida de todos los pokemones");
+                Console.WriteLine("3- Curar un pokemón");
+                Console.WriteLine("4- Dañar un pokemon");
+                Console.WriteLine("5- Curar a todos los pokemones");
+                Console.WriteLine("6- Mostrar pokemones debilitados");
+                Console.WriteLine("7- Mostrar el pokemon con mayor vida");
+                Console.WriteLine("8- Mostrar el pokemon con menor vida");
+                Console.WriteLine("9- Calcular promedio de vida del equipo");
+                Console.WriteLine("10- Ordenar pokemones por vida de menor a mayor");
+                Console.WriteLine("11- Ordenar pokemones por vida de mayor a menor");
+                Console.WriteLine("12- Simular ataque enemigo a todo el equipo");
+                Console.WriteLine("13- Salir");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            if (enLista < 6)
+                            {
+                                Console.Write("Ingrese la vida del pokemon: ");
+                                vidasPokemones[siguiente] = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Pokemon registrado correctamente en la posición " + siguiente);
+                                siguiente++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se pueden registrar más pokemones, el centro está lleno");
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            for (int i = 0; i < vidasPokemones.Length; i++)
+                            {
+                                Console.WriteLine("Pokemon " + (i + 1) + " - Vida: " + vidasPokemones[i]);
+                            }
+                            break;
+                        }
+                }
                 Console.ReadKey();
+                Console.Clear();
             }
-            switch(opcion)
-            {
-                case 1:
-                    {
-
-                        break;
-                    }
-            }
-            Console.ReadKey();
         }
     }
 }
