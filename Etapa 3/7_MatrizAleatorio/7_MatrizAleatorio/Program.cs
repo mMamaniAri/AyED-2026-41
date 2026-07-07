627931da78b3ae962744939a4afc63f756e3a4ab
Matias Mamani <matyescolar391@gmail.com>
2026-07-07 11:22:30 -0300

    subo progreso etapa 3

---
+++ Etapa 3/7_MatrizAleatorio/7_MatrizAleatorio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_MatrizAleatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int n;
+            int m;
+            Console.Write("Ingrese la cantidad de filas: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese la cantidad de columnas: ");
+            m = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matriz[i, j] = rand.Next(1, 100);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 3/8_SumandoMatrices/8_SumandoMatrices/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_SumandoMatrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int n;
+            Console.Write("Ingrese el tamaño de la matriz: ");
+            n = int.Parse(Console.ReadLine());
+            int[,] matriz= new int[n, n];
+            int[,] matriz2= new int[n, n];
+            int[,] matriz3 = new int[n, n];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = rand.Next(1, 100);
+                    matriz2[i, j] = rand.Next(1, 100);
+                }
+            }
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    matriz3[i, j] = matriz[i, j] + matriz2[i, j];
+                    Console.Write(matriz3[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
